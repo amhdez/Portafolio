a5ebc5687380ae0ac066271b37e4a2012a564880
AnaHernandez <amhdez079@gmail.com>
2024-05-01 16:52:45 -0500

    No corresponden al portafolio

--- JAVASCRIPT/Ejercicio.js
+++
@@ -1,8 +0,0 @@
-
-let numeroEntero = prompt("Ingrese un numero entero positivo: ");
-
-if (numeroEntero >= 2 ){prompt("El numero ingresado es positivo y mayor a dos: ");
-} else if (numeroEntero < 2); prompt("Error: Debes ingresar un número entero positivo mayor o igual a 2");
-do { 
-    let numeroEntero = prompt("Ingrese un numero válido: " ); 
-}
